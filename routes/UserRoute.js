aa649ac6ceb171a91b802756eb4e580b98ccb5f9
mitrukahitesh <mitrukahitesh@gmail.com>
2023-01-04 15:56:51 +0530

    avatar update end point updated

--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -43,13 +43,9 @@ app.post("/user", auth.loggedIn, async (req, res) => {
 app.put("/user", auth.loggedIn, async (req, res) => {
   const email = req.email;
   try {
-    if (email !== req.body.email) {
-      res.status(401).send({ message: "Primary email can't be updaed" });
-      return;
-    }
     const user = await userModel.findOne({ email: email }).exec();
-    await user.update(req.body);
-    res.status(200).send({ ...req.body, admin: [] });
+    await user.updateOne(req.body);
+    res.status(200).send(req.body);
   } catch (error) {
     console.log(error);
     res.status(500).send(error);
