c2847d50eff003ffd5258609f3d7300f8600b657
mitrukahitesh <mitrukahitesh@gmail.com>
2023-01-07 13:50:49 +0530

    added subscribe/unsibscribe endpoints

--- models/UserModel.js
+++ models/UserModel.js
@@ -39,6 +39,10 @@ const UserSchema = new mongoose.Schema(
     avatar: {
       type: String,
     },
+    subscribed: {
+      type: Array,
+      default: [],
+    },
   },
   { versionKey: false }
 );
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -24,11 +24,6 @@ app.get("/user", auth.loggedIn, async (req, res) => {
   }
 });
 
-async function getAdminList(email) {
-  const user = await admin.auth().getUserByEmail(email);
-  return user.hasOwnProperty("customClaims") ? user.customClaims : {};
-}
-
 app.post("/user", auth.loggedIn, async (req, res) => {
   const user = new userModel(req.body);
   try {
@@ -51,5 +46,34 @@ app.put("/user", auth.loggedIn, async (req, res) => {
     res.status(500).send(error);
   }
 });
+
+app.put("/user/subscribe", auth.loggedIn, async (req, res) => {
+  const email = req.email;
+  try {
+    const user = await userModel.findOne({ email: email }).exec();
+    await user.updateOne({ $addToSet: { subscribed: req.body.club } });
+    res.status(200).send(req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+app.put("/user/unsubscribe", auth.loggedIn, async (req, res) => {
+  const email = req.email;
+  try {
+    const user = await userModel.findOne({ email: email }).exec();
+    await user.updateOne({ $pull: { subscribed: req.body.club } });
+    res.status(200).send(req.body);
+  } catch (error) {
+    console.log(error);
+    res.status(500).send(error);
+  }
+});
+
+async function getAdminList(email) {
+  const user = await admin.auth().getUserByEmail(email);
+  return user.hasOwnProperty("customClaims") ? user.customClaims : {};
+}
 
 module.exports = app;
