08770e43bce86372a6554d4836672c963b8ed810
Shashank Verma <shashank.verma2002@gmail.com>
2023-02-10 23:32:43 +0530

    Added API end - points for URLs of clubs
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ app/services/UrlService.js
@@ -0,0 +1,34 @@
+const urlRepository = require("../repository/UrlRepository");
+const validate = require("../utility/validate");
+
+const getUrls = async (clubId) => {
+    validate([clubId]);
+
+    const urls = await urlRepository.getUrls(clubId);
+    return urls;
+};
+
+const saveUrl = async (url) => {
+    validate([url.clubId, url.name, url.color, url.url]);
+
+    await urlRepository.saveUrl(url);
+};
+
+const updateUrl = async (urlId, url) => {
+    validate([urlId, url.name, url.color, url.url]);
+
+    await urlRepository.updateUrl(urlId, url);
+};
+
+const deleteUrl = async (urlId) => {
+    validate([urlId]);
+
+    await urlRepository.deleteUrl(urlId);
+};
+
+module.exports = {
+    getUrls,
+    saveUrl,
+    updateUrl,
+    deleteUrl,
+};
