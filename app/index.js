3670881a5efbac7ee9dca642e8023f29d8adab4d
mitrukahitesh <mitrukahitesh@gmail.com>
2023-02-17 15:35:06 +0530

    fixed import error

--- app/index.js
+++ app/index.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use("/user", require("./routes/UserRoute"));
 app.use("/clubs", require("./routes/ClubRoute"));
 app.use("/posts", require("./routes/PostRoute"));
-app.use("/admin", require("./routes/AdminRoute"));
+app.use("/admin", require("./routes/SuperAdminRoute"));
 app.use("/channel", require("./routes/ChannelRoute"));
 app.use("/url", require("./routes/UrlRoute"));
 app.use("/views", require("./routes/ViewRoute"));
