6bec577d07c6dd929f287b46257fd68d72147944
Shashank Verma <shashank.verma2002@gmail.com>
2023-04-19 14:12:00 +0530

    Fix build issues
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/index.ts
+++ app/index.ts
@@ -1,5 +1,5 @@
-import express = require("express");
-import cors = require("cors");
+import * as express from 'express';
+import * as cors from 'cors';
 
 import { ChannelRouter } from "./routes/ChannelRoute";
 import { ClubRouter } from "./routes/ClubRoute";
