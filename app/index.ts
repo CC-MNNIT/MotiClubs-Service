aeaa2a349902bf400716e50e587c7562ad909187
Shashank Verma <shashank.verma2002@gmail.com>
2023-06-02 09:59:42 +0530

    index: Change prefix of root url
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- app/index.ts
+++ app/index.ts
@@ -1,17 +1,17 @@
-import * as express from 'express';
 import * as cors from 'cors';
 import * as dotenv from 'dotenv';
+import * as express from 'express';
 
 dotenv.config();
 
 import { ChannelRouter } from "./routes/ChannelRoute";
 import { ClubRouter } from "./routes/ClubRoute";
 import { PostRouter } from "./routes/PostRoute";
+import { ReplyRouter } from './routes/ReplyRoute';
 import { SuperAdminRouter } from "./routes/SuperAdminRoute";
 import { UrlRouter } from "./routes/UrlRoute";
 import { UserRouter } from "./routes/UserRoute";
 import { ViewRouter } from "./routes/ViewRoute";
-import { ReplyRouter } from './routes/ReplyRoute';
 
 const app: express.Application = express();
 
@@ -20,14 +20,14 @@ app.use(express.json());
 
 const rootUrl: string = process.env.ROOT_URL || "";
 
-app.use(`/${rootUrl}user`, UserRouter);
-app.use(`/${rootUrl}clubs`, ClubRouter);
-app.use(`/${rootUrl}posts`, PostRouter);
-app.use(`/${rootUrl}admin`, SuperAdminRouter);
-app.use(`/${rootUrl}channel`, ChannelRouter);
-app.use(`/${rootUrl}url`, UrlRouter);
-app.use(`/${rootUrl}views`, ViewRouter);
-app.use(`/${rootUrl}reply`, ReplyRouter);
-app.use(`/${rootUrl}`, (_, res) => res.status(200).send({ message: "API Running" }));
+app.use(`${rootUrl}/user`, UserRouter);
+app.use(`${rootUrl}/clubs`, ClubRouter);
+app.use(`${rootUrl}/posts`, PostRouter);
+app.use(`${rootUrl}/admin`, SuperAdminRouter);
+app.use(`${rootUrl}/channel`, ChannelRouter);
+app.use(`${rootUrl}/url`, UrlRouter);
+app.use(`${rootUrl}/views`, ViewRouter);
+app.use(`${rootUrl}/reply`, ReplyRouter);
+app.use(`${rootUrl}/`, (_, res) => res.status(200).send({ message: "API Running user" }));
 
 export default app;
