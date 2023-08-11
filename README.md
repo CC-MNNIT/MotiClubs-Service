f2049cf321f9f4369dfe38e310a14be61e0346c4
Shashank Verma <shashank.verma2002@gmail.com>
2023-08-11 12:55:09 +0530

    Release version 1.0.0
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

---
+++ README.md
@@ -0,0 +1,38 @@
+# MotiClubs - Service
+
+This service is backend of the [MotiClubs App](https://github.com/CC-MNNIT/MotiClubs).
+
+## Swagger URLs
+
+| Environment | URL                                       |
+|-------------|-------------------------------------------|
+| dev         | http://localhost:8002/swagger             |
+| prod        | https://sac.mnnit.ac.in/moticlubs/swagger |
+
+## Testing
+
+This service additionally requires the following files for running locally:
+
+- `r2dbc.yml`
+- `firebase_private_key.json`
+
+### Run Locally
+
+Execute the service
+
+```shell
+mvn spring-boot:run -Dspring-boot.run.arguments="--spring.profiles.active=dev"
+```
+
+Packaging jar file
+
+```shell
+mvn clean package
+```
+
+### Run prod
+
+```shell
+mvn clean package
+java -DLog4jContextSelector=org.apache.logging.log4j.core.async.AsyncLoggerContextSelector -XX:+UseSerialGC -XX:MaxRAMPercentage=75.0 -jar MotiClubs-Service.jar --spring.profiles.active=prod
+```
--- src/main/kotlin/com/mnnit/moticlubs/OpenAPIDefinition.kt
+++ src/main/kotlin/com/mnnit/moticlubs/OpenAPIDefinition.kt
@@ -2,9 +2,11 @@ package com.mnnit.moticlubs
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType
+import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.security.SecurityScheme
+import io.swagger.v3.oas.annotations.servers.Server
 
 @SecurityScheme(
     type = SecuritySchemeType.HTTP,
@@ -15,7 +17,22 @@ import io.swagger.v3.oas.annotations.security.SecurityScheme
     info = Info(
         title = "MotiClubs Backend",
         description = "Backend to provide service of the app",
+        contact = Contact(
+            name = "CC-Club MNNIT",
+            url = "https://github.com/CC-MNNIT",
+            email = "computer.club@mnnit.ac.in"
+        ),
     ),
+    servers = [
+        Server(
+            url = "http://localhost:8002/",
+            description = "Localhost",
+        ),
+        Server(
+            url = "https://sac.mnnit.ac.in/moticlubs/",
+            description = "Production",
+        ),
+    ],
     security = [SecurityRequirement(name = "Firebase Auth")]
 )
 class OpenAPIDefinition
