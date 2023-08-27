55aa675396f7f9f64b50d43ec281ff7c123dc26e
Shashank Verma <shashank.verma2002@gmail.com>
2023-08-27 15:59:41 +0530

    Enable caching for replies
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/main/kotlin/com/mnnit/moticlubs/service/ReplyService.kt
+++ src/main/kotlin/com/mnnit/moticlubs/service/ReplyService.kt
@@ -3,6 +3,8 @@ package com.mnnit.moticlubs.service
 import com.mnnit.moticlubs.dao.Reply
 import com.mnnit.moticlubs.repository.ReplyRepository
 import com.mnnit.moticlubs.utils.UnauthorizedException
+import org.springframework.cache.annotation.CacheEvict
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
@@ -13,16 +15,19 @@ class ReplyService(
     private val notificationService: NotificationService,
 ) {
 
+    @CacheEvict("replies", allEntries = true)
     fun saveReply(reply: Reply): Mono<Reply> = replyRepository.save(reply)
         .flatMap { savedReply ->
             notificationService.notifyReply(savedReply)
                 .then(Mono.just(savedReply))
         }
 
+    @Cacheable("replies", key = "#pid + '_' + #pageRequest.pageNumber")
     fun getRepliesByPid(pid: Long, pageRequest: PageRequest): Mono<List<Reply>> = replyRepository
         .findAllByPid(pid, pageRequest)
         .collectList()
 
+    @CacheEvict("replies", allEntries = true)
     fun deleteReply(uid: Long, time: Long): Mono<Void> = replyRepository.findById(time)
         .flatMap { reply ->
             if (reply.uid != uid) {
