73c17e60100c1a23b6ebeb62139fd6624480558a
Shashank Verma <shashank.verma2002@gmail.com>
2023-09-30 10:20:17 +0530

    openApi: Set servers based on env profile
    Signed-off-by: Shashank Verma <shashank.verma2002@gmail.com>

--- src/main/kotlin/com/mnnit/moticlubs/OpenAPIDefinition.kt
+++ src/main/kotlin/com/mnnit/moticlubs/OpenAPIDefinition.kt
@@ -1,29 +1,38 @@
 package com.mnnit.moticlubs
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition
-import io.swagger.v3.oas.annotations.info.Contact
-import io.swagger.v3.oas.annotations.info.Info
-import io.swagger.v3.oas.annotations.servers.Server
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
+import org.springdoc.core.customizers.OpenApiCustomizer
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.core.env.Environment
 
-@OpenAPIDefinition(
-    info = Info(
-        title = "MotiClubs Service",
-        description = "Backend to provide service to the MotiClubs App",
-        contact = Contact(
-            name = "CC-Club MNNIT",
-            url = "https://github.com/CC-MNNIT",
-            email = "computer.club@mnnit.ac.in",
-        ),
-    ),
-    servers = [
-        Server(
-            url = "http://localhost:8002/",
-            description = "Localhost",
-        ),
-        Server(
-            url = "https://sac.mnnit.ac.in/moticlubs/",
-            description = "Production",
-        ),
-    ],
-)
-class OpenAPIDefinition
+@Configuration
+class OpenAPIDefinition {
+
+    @Bean
+    fun openApiConfig(environment: Environment): OpenApiCustomizer = OpenApiCustomizer {
+        it.info = Info().apply {
+            title = "MotiClubs Service"
+            description = "Backend to provide service to the MotiClubs App"
+            contact = Contact().apply {
+                name = "CC-Club MNNIT"
+                url = "https://github.com/CC-MNNIT"
+                email = "computer.club@mnnit.ac.in"
+            }
+        }
+
+        it.servers = listOf(
+            Server().apply {
+                if (environment.activeProfiles.contains("prod")) {
+                    url = "https://sac.mnnit.ac.in/moticlubs/"
+                    description = "Production"
+                } else {
+                    url = "http://localhost:8002/"
+                    description = "Localhost"
+                }
+            },
+        )
+    }
+}
